49dc4eebf3dd7ca895f00f25481d98e961d723ae
Sabelo Mdashe <selbysabelomdashe@gmail.com>
2022-06-17 16:12:15 +0200

    added more projects to the projects page

---
+++ index.js
@@ -0,0 +1,66 @@
+// PROJECTS
+const projects = [
+  {
+    title: "Personal Portfolio",
+    description: "Lorem ipsum whatever",
+    techUsed: "HTML/CSS/Bootstrap",
+    github: "https://github.com/Sabelo-Mdashe/Selby_Portfolio",
+    live: "https://stirring-daffodil-11a73e.netlify.app/",
+    imgURL: "https://postimg.cc/bdVj02td",
+  },
+  {
+    title: "Mock Portfolio",
+    description: "Lorem ipsum bluh bluh",
+    techUsed: "HTML/CSS",
+    github: "https://github.com/Sabelo-Mdashe/Mock-Portfolio",
+    live: "https://ephemeral-pegasus-115bcf.netlify.app/",
+    imgURL: "https://postimg.cc/064BM73q",
+  },
+];
+
+// RESUME
+let resume = [
+  {
+    date: "03/2022-Current",
+    place: "Life Choices",
+    occupation: "Intern",
+    responsibilities: ["Coding", "Presentations", "Web Development"],
+  },
+  {
+    date: "09/2020-03/2021",
+    place: "Younglings Africa",
+    occupation: "Intern",
+    responsibilities: ["Coding", "Presentations", "Problem Solving"],
+  },
+  {
+    date: "03/2020-08/2020",
+    place: "CapaCiTi",
+    occupation: "Intern",
+    responsibilities: ["Coding", "Presentations", "Application Development"],
+  },
+];
+
+// TESTIMONIALS
+let testimonials = [
+  {
+    name: "Godwin Dzvapatsva",
+    testimonial:
+      "Sabelo is interested and engaged in his lessons and always completes his projects on time. He has a gentle personality, which allows him to work with any of his classmates. Sabelo has proven to have a vital grasp of the HTML, CSS, and Bootstrap topics we have covered in class. Sabelo is such a smart student that he should continue to do well.",
+    occupation: "Lecturer",
+    profile: "",
+  },
+  {
+    name: "Le-Andre Moritz",
+    testimonial:
+      "Sabelo is a diligent worker, always stays focused and works hard. He has a good attention for detail and a keen eye for neat and professional design.",
+    occupation: "Collegue",
+    profile: "",
+  },
+  {
+    name: "Lunga Booi",
+    testimonial:
+      "Sabelo is a good person to work with. He has passion for his work and likes to explore new ideas. He is very intellectual. He can manage his time and work with new people.",
+    occupation: "Collegue",
+    profile: "",
+  },
+];
